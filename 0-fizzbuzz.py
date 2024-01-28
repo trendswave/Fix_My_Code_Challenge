1bbb6cea6ebc5e0fba2a2c5c5c02707d973d66cd
trendswave <paulchinedumicheal@gmail.com>
2024-01-28 21:41:12 +0100

    fixed the error on fizbuzz

---
+++ 0-fizzbuzz.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" FizzBuzz
+"""
+import sys
+
+
+def fizzbuzz(n):
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of three, print "Fizz" instead of the number.
+    - For multiples of five, print "Buzz" instead of the number.
+    - For numbers which are multiples of both three and five, print "FizzBuzz".
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:  # Check multiples of both 3 and 5 first
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:  # Check multiples of 3
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:  # Check multiples of 5
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))  # Convert non-multiples to string
+    print(" ".join(tmp_result))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
+
